3477ed9343bf83b2dd9960b4cbee4bcd152632b6
ColdWindScholar <3590361911@qq.com>
2024-02-12 21:52:17 +0800

    Dont  Use glob

--- pyscripts/utils.py
+++ pyscripts/utils.py
@@ -58,10 +58,7 @@ def gettype(file) -> str:
 
 
 def mkdir(path):
-    if not os.path.exists(path):  # 判断是否存在文件夹如果不存在则创建为文件夹
-        os.makedirs(path)  # makedirs 创建文件时如果路径不存在会创建这个路径
-    else:
-        return False
+    os.makedirs(path) if not os.path.exists(path) else ...
 
 
 def unzip_file(zip_src, dst_dir):
