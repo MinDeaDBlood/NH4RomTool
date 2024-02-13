6d831aecf1a3fae024b7cdae0b87cd85ed2bda6f
ColdWindScholar <3590361911@qq.com>
2024-02-13 12:29:51 +0800

    Dont  Use glob

--- build.py
+++ build.py
@@ -42,7 +42,7 @@ def zip_folder(folder_path):
 
 import PyInstaller.__main__
 
-PyInstaller.__main__.run(['-F', 'ui.py', '--exclude-module=numpy', '-i', 'bin/logo.ico'])
+PyInstaller.__main__.run(['-Fw', 'ui.py', '--exclude-module=numpy', '-i', 'bin/logo.ico'])
 
 if os.path.exists(local + os.sep + "dist" + os.sep + "ui.exe"):
     shutil.move(local + os.sep + "dist" + os.sep + "ui.exe", local)
