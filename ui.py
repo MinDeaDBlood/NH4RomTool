de0407efb30eee200237f43fd889060d69f2d221
ColdWindScholar <3590361911@qq.com>
2024-02-14 12:59:39 +0800

    [24.2.13]

--- ui.py
+++ ui.py
@@ -983,11 +983,7 @@ if __name__ == '__main__':
         cmd = usercmd.get()
         if not cmd:
             return
-        for i in ["bash", 'ash', 'cmd']:
-            if i in cmd.split()[0]:
-                print("这种命令会阻塞窗口， 所以终止执行")
-                return
-        runcmd("busybox ash -c \"%s\"" % cmd)
+        cz(runcmd, "busybox ash -c \"%s\"" % cmd)
         usercmd.delete(0, 'end')
 
 
